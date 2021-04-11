2da9a2d60d0991c878e5908e70c009f56e297219
Junhee Ko <jko@rainist.com>
2021-04-11 13:48:45 +0900

    Fix test case fail

--- src/main/java/refiner/TextRefiner.java
+++ src/main/java/refiner/TextRefiner.java
@@ -5,9 +5,9 @@ public class TextRefiner {
   public String refine(String s) {
     return s
         .replace("    ", " ")
+        .replace("\t", " ")
         .replace("  ", " ")
         .replace("  ", " ")
-        .replace("  ", " ")
-        .replace("\t ", " ");
+        .replace("  ", " ");
   }
 }
