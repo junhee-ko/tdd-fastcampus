398beaf8e679e021dcf76ac4361dcfbf3316ecbf
Junhee Ko <jko@rainist.com>
2021-04-18 14:13:56 +0900

    Refactor VarianceCalculator

--- src/main/java/variance/VarianceCalculator.java
+++ src/main/java/variance/VarianceCalculator.java
@@ -3,36 +3,51 @@ package variance;
 public class VarianceCalculator {
 
   public static void main(String[] args) {
-    int n = args.length;
 
-    if (n == 0) {
-      System.out.println("입력된 데이터가 없습니다.");
-      return;
+    switch (args.length) {
+      case 0:
+        System.out.println("입력된 데이터가 없습니다.");
+        break;
+      case 1:
+        System.out.println("데이터가 부족해 분산을 계산할 수 없습니다. 2개 이상의 데이터를 입력해 주세요.");
+        break;
+      default:
+        System.out.println("분산 : " + getVariance(args));
     }
+  }
 
-    if (n == 1) {
-      System.out.println("데이터가 부족해 분산을 계산할 수 없습니다. 2개 이상의 데이터를 입력해 주세요.");
-      return;
-    }
+  private static double getVariance(String[] args) {
+    double[] source = parseArguments(args);
+    double mean = calculateMean(source);
+    double sumOfSquares = calculateSumOfSquares(args, source, mean);
 
-    double[] source = new double[n];
-    for (int i = 0; i < n; i++) {
-      source[i] = Double.parseDouble(args[i]);
-    }
+    return sumOfSquares / (args.length - 1);
+  }
 
-    double sum = 0.0;
-    for (int i = 0; i < n; i++) {
-      sum += source[i];
-    }
-
+  private static double calculateSumOfSquares(String[] args, double[] source, double mean) {
     double sumOfSquares = 0.0;
-    double mean = sum / n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < args.length; i++) {
       sumOfSquares += (source[i] - mean) * (source[i] - mean);
     }
 
-    double variance = sumOfSquares / (n - 1);
+    return sumOfSquares;
+  }
 
-    System.out.println("분산 : " + variance);
+  private static double calculateMean(double[] source) {
+    double sum = 0.0;
+    for (double value : source) {
+      sum += value;
+    }
+
+    return sum / source.length;
+  }
+
+  private static double[] parseArguments(String[] args) {
+    double[] source = new double[args.length];
+    for (int i = 0; i < args.length; i++) {
+      source[i] = Double.parseDouble(args[i]);
+    }
+
+    return source;
   }
 }
